3f548b922c90831d2304c3dcfea394c780c5435a
Nicole D <nnardella@gmail.com>
2019-01-10 14:57:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def Show::most_popular_show
-    self.where('rating').order(rating: :desc).first
+    self.where("rating").order(rating: :desc).first
     binding.pry
   end
 
