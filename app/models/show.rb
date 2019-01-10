9b8fb694a96a30f7e99f41253afdee3f11e82dfa
Nicole D <nnardella@gmail.com>
2019-01-10 15:25:26 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    self.maximum(:rating)
+  end
+
+  def Show::most_popular_show
+    self.where("rating").order(rating: :desc).first
+  end
+
+  def Show::lowest_rating
+    self.minimum(:rating)
+  end
+
+  def Show::least_popular_show
+    self.where("rating").order(rating: :asc).first
+  end
+
+  def Show::ratings_sum
+    self.sum(:rating)
+  end
+
+  def Show::popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def Show::shows_by_alphabetical_order
+    self.order(:name)
+  end
+end
